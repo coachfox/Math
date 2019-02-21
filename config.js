b383f8dbb17c2a96dbd45bd156e74f9647940c00
ben sharp <3467167776@qq.com>
2019-02-21 14:09:42 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    serverPort: '8124',
+    //database信息
+    database:'allcontent',
+    databaseurl:'mongodb://localhost:27017/',
+    // 小程序 appId 和 appSecret
+    // 请到 https://mp.weixin.qq.com 获取 AppID 和 AppSecret
+    appId: 'wxa1756b97cfee22c0',
+    appSecret: '41972d18ed17ccbcfdedbc3f506d21d7',
+    // mongodb 连接配置，生产环境请使用更复杂的用户名密码
+    mongoHost: '127.0.0.1',   
+    myHost:"http://127.0.0.1:8124",
+    mongoPort: '27017',
+    mongoUser: 'weapp',
+    mongoPass: 'weapp-dev',
+    mongoDb: 'weapp',
+    //路由信息
+    mainpage:'/index', 
+    mianpagehtml:'./htmls/htmlcontent/index.js',
+    getshpname:'/getshpname',
+    submitspzl:'/submitspzl', 
+    uploadfile:'/uploadFile',
+    getshpzl:'/getshpzl',
+    getscriptdelshp:'/getscriptdelshp' ,
+    delshpzl:'/delshpzl',
+    uploadfileid:'/uploadfileid',
+    lgetshpzl:"/lgetshpzl",
+    test:"/test",
+    zuishu:"/zuishu",
+    zuishuweb:"/zuishuweb",
+    zuishuget:"/zuishuget",
+    submitzhuisuzl:"/submitzhuisuzl",
+    aishangconsole:"/aishangconsole",
+    inserttczl:"/inserttczl",
+    getalltczl:"/getalltczl",
+    deltczl:"/deltczl",
+    serverPort: '8765', 
+
+    // 小程序 appId 和 appSecret 
+    // 请到 https://mp.weixin.qq.com 获取 AppID 和 AppSecret
+    appId: 'wxa1756b97cfee22c0', 
+    appSecret: 'af296709cc3f7518fe511a2891a1d8bd', 
+
+    // mongodb 连接配置，生产环境请使用更复杂的用户名密码
+    mongoHost: '127.0.0.1', 
+    mongoPort: '27017', 
+    mongoUser: 'weapp', 
+    mongoPass: 'weapp-dev', 
+    mongoDb: 'weapp'
+};
+
